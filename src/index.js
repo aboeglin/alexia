f855a887c934fa0a381c99b9385151cb2100d611
alexiasch <schneideralexia1@gmail.com>
2024-08-09 20:51:41 +0200

    fix deploy3

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send({ message: "hello world" });
+  res.send("<h1>hello world</h1>");
 });
 
 app.listen(port, () => {
